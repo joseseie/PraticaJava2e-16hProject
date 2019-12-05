2e7a51a5f0217e0a59b0fd121c10622dfa67dcbd
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-05 18:37:25 +0200

    Adicao de exercicios

--- src/mz/e/exercicios/Aula04.java
+++ src/mz/e/exercicios/Aula04.java
@@ -12,4 +12,48 @@ package mz.e.exercicios;
  */
 public class Aula04 {
     
+    public static void main(String[] args) {
+        // 1. Analise as declaracoes e inicializacoess de variavies a baixo e faca alguma correcao se necessario
+
+
+        int numero="um";
+
+        string 1 ="um";
+
+        float x="0.2":
+
+        string x='nome';
+
+        string .ab.com ="site";
+
+        boolean z="true";
+
+        double v=0,5,
+
+        String NomeAluno="Joao"
+
+        int idade.aluno=18;
+
+        String Jõao;
+        
+        int Intenger=2;
+
+        double valor_pago=true;
+
+        int numero1=2,2;
+
+        boolean _Valr=66
+
+        Integer int = 10
+        
+        char char = 'K'
+        
+        char char;
+        
+        Doubles salario.
+        
+        String MEU_NOME = 'Esse é o meu nome'
+        
+        public long numeroDaConta = 123456789123456789;    
+    }
 }
