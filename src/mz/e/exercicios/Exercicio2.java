6e1ee78a10af9fc34af061bf0fd095bf726cc54e
Efraime Mutole <58521063+mutole@users.noreply.github.com>
2019-12-06 17:24:16 +0200

    Update Exercicio2.java

--- src/mz/e/exercicios/Exercicio2.java
+++ src/mz/e/exercicios/Exercicio2.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 2.	Faça um programa que imprima a frase: "Seja bem-vindo ao meu programa!", Em seguida, imprima 
+        uma linha em branco e na sequência, imprima as frases e o resultado da expressões matemáticas.
+        “O produto de 8 por 90 é igual a:”
+        “O quociente de 90 por 30 é igual a:”
+        “O quadrado de 9 é: “
+    Cada frase e seu resultado devem estar na mesma linha.
+    
  */
 package mz.e.exercicios;
 
@@ -10,5 +14,28 @@ package mz.e.exercicios;
  * @author Mutole
  */
 public class Exercicio2 {
+    
+    public static void main(String args [])
+    {
+        //DECLARACOES DAS VARIAVEIS
+        byte n1= 8, n2=90;
+        int produto= n1*n2;
+        
+        byte n3= 30;
+        float quo= n2/n3;
+        
+        byte qua= 9*9;
+        
+       System.out.println("Seja bem vindo ao meu programa");
+        
+       System.out.println("O produto do numero 8 por 90 e':" +produto);
+       
+       System.out.println("O quociente do numero 90 por 30 e':" +quo);
+       
+       System.out.println("O quadrado do numero 9 e':" +qua);
+       
+        
+        
+    }
     
 }
