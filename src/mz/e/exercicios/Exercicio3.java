5fd2d8269973cb19d7a0ae6017b3378b50654b74
Efraime Mutole <58521063+mutole@users.noreply.github.com>
2019-12-06 17:29:34 +0200

    Create Exercicio3.java

---
+++ src/mz/e/exercicios/Exercicio3.java
@@ -0,0 +1,14 @@
+/*
+  3.	Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, 
+        imprima na tela as 2 informações a seguir:
+                Eu chamo me xxxxx de idade xxxxx anos de idade, estudante de xxxxx.
+ */
+package mz.e.exercicios;
+
+/**
+ *
+ * @author Mutole
+ */
+public class Exercicio3 {
+    
+}
