9b561c5af59808b460ad0f476cb76fdf8aaed7e2
Efraime Mutole <58521063+mutole@users.noreply.github.com>
2019-12-06 17:03:28 +0200

    Resolucao do exercicio 1

---
+++ src/mz/e/exercicios/Exercicio1.java
@@ -0,0 +1,25 @@
+/*
+ 
+    Lista de Exercícios de Aplicação
+    1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na 
+        tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. 
+        Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+        "Seja bem-vindo ao curso de Java Básico Intensivo"
+ */
+package mz.e.exercicios;
+
+/**
+ *
+ * @author Mutole
+ */
+public class Exercicio1 {
+    
+    public static void main(String args [])
+    {
+        System.out.println("Ola Mundo");
+        System.out.println("efraimemutole26@gmail.com");
+        System.out.println("");
+        System.out.println("Seja bem vindo ao curso de Java Intesivo");
+    }
+    
+}
