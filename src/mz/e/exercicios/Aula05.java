8c94edb746d21de9c8d9311ed0ea87df30059829
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-06 15:03:38 +0200

    Encurtamento de linhas

--- src/mz/e/exercicios/Aula05.java
+++ src/mz/e/exercicios/Aula05.java
@@ -13,19 +13,23 @@ public class Aula05 {
     /*
     
     Lista de Exercícios de Aplicação
-    1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+    1.	Faça um programa que imprima o texto “Olá  mundo" para o usuário. Em seguida, imprima na 
+        tela o seu código de visita da plataforma Explicador e então, imprima uma linha em branco. 
+        Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
         "Seja bem-vindo ao curso de Java Básico Intensivo"
     
-    2.	Faça um programa que imprima a frase: "Seja bem-vindo ao meu programa!", Em seguida, imprima uma linha em branco e na sequência, imprima as frases e o resultado da expressões matemáticas.
+    2.	Faça um programa que imprima a frase: "Seja bem-vindo ao meu programa!", Em seguida, imprima 
+        uma linha em branco e na sequência, imprima as frases e o resultado da expressões matemáticas.
         “O produto de 8 por 90 é igual a:”
         “O quociente de 90 por 30 é igual a:”
         “O quadrado de 9 é: “
     Cada frase e seu resultado devem estar na mesma linha.
     
-    3.	Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, imprima na tela as 2 informações a seguir:
+    3.	Escreva um pequeno programa que peça o seu nome, sua ocupação e ano de nascimento. Em seguida, 
+        imprima na tela as 2 informações a seguir:
                 Eu chamo me xxxxx de idade xxxxx anos de idade, estudante de xxxxx.
     
-    4.	Faça um programa que imprima o seu nome e imprima em tela
+    4.	Faça um programa que leia o seu nome e imprima em tela
     
     5.	Faça um programa em leia seu nome e apelido e imprima o seu nome completo
     
@@ -40,13 +44,16 @@ public class Aula05 {
     
     10.	Faça um algoritmo que leia dois números e mostre qual o maior dos dois
     
-    11.	Crie um programa que calcule a média de um aluno para a disciplina de programação. O aluno deve informar seu nome e as três notas da matéria.
+    11.	Crie um programa que calcule a média de um aluno para a disciplina de programação. 
+        O aluno deve informar seu nome e as três notas da matéria.
     
-    12.	Fazer um programa que imprima a média aritmética dos números 8,9 e 7. A média dos números 4, 5 e 6. A soma das duas médias. A média das médias.
+    12.	Fazer um programa que imprima a média aritmética dos números 8,9 e 7. 
+        A média dos números 4, 5 e 6. A soma das duas médias. A média das médias.
     
     13.	Informar um saldo e imprimir o saldo com o aumento de 1%.
     
-    14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
+    14.	Escreva uma classe ConversaoDeUnidadesDeTemperatura que contenha métodos estáticos para 
+        calcular a conversão entre diferentes escalas de temperatura. Considere as fórmulas de conversão abaixo: 
     a)	De graus Celsius (C) para graus Fahrenheit (F): F = (9 × C/5) + 32) 
     b)	De graus Fahrenheit (F) para graus Celsius (C): C = (F − 32) × +5/9 
     c)	De graus Celsius (C) para graus Kelvin (K): K = C + 273.15 
