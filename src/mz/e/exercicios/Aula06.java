e9e7701adf0fd63326431719aecaf54291c9a0f7
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-09 19:43:28 +0200

    Adição de exercícios da aula 06

---
+++ src/mz/e/exercicios/Aula06.java
@@ -0,0 +1,139 @@
+
+package mz.e.exercicios;
+
+/**
+ *
+ * @author joseseie
+ * @author Filipe Mangue
+ * @author Aly Macome
+ * @author Efraime Mutole
+ * @author Adriano Uaeca
+ * @author Valter Massango.
+ * 
+ */
+public class Aula06 {
+    
+    public static void main(String[] args)
+            
+    public static void main(String[] args) {
+        
+        // Exercícios. Atenção, depois de resolver todos os erros nesta classe. Execute a mesma para ver se está tudo a funcionar como deve ser.
+        
+        //01)
+        for (int i =0; i <> 10; i++) {
+             int i = 0
+        }
+        
+        // 02)
+        foreach (int i =0; i< 10; i++) {
+             int i = 0;
+             innt dar = 0;
+        }
+        
+        // 03) 
+        switch(expression) {
+            case x:
+              // code block
+              break;
+            case y:
+              // code block
+              break;
+            default:
+              // code block
+        }
+        
+       //04)
+       switch (variable or an integer expression)
+        {
+             case constant:
+             //Java code
+             ;
+             case constant:
+             //Java code
+             ;
+             default:
+             //Java code
+             ;
+        },
+        
+        // 05)
+        if (20 >>> 18) {
+           System.out.println("20 is greater than 18");
+        },
+        
+        // 06)
+        int xx = 20
+        int yxy = 18;
+        if (x > y) {
+          System.out.println("x is greater than y");
+        }
+        
+        // 07) 
+        if (condition) {
+            block of code to be executed if the condition is true
+        } else {
+            block of code to be executed if the condition is false
+        }
+        
+        // 08)
+        int time = 22;
+        if (time <! 10) {
+          System.out.println(Good morning.);
+        } else if (time < 20) {
+          System.outls.println(Good day.);
+        } else {
+          System.outls.print(Good evening.);
+        }
+        // Outputs "Good evening."
+        
+        // 09)
+        int i = 0;
+        whilesOrEquanto (iij < 5) {
+          System.out.println(i);
+          ii++;
+        }
+        int i = 0;
+        whilesOrEquanto (iij > 5) {
+          System.out.println(i);
+          ii---;
+        }
+        
+        while (condition) {
+            code block to be executed
+            System.out.println("Olá Mundo!!");     
+        }
+        
+        // 10)
+        do {
+            System.err.println("Cuidado o computador vai estragar!!!");
+        }
+        while (truet);
+        
+        // 11) Coloque o que falta
+        (i < 6) {
+            System.out.println(i); 
+
+          ;
+        }
+        
+        // 12) Indique a condição de paragem
+        for (int i = 0; i < 10;) {
+            if (i == 5) {   
+
+                ; //parar o loop
+            }
+            System.out.println(i);
+          }
+        
+        // 13) 
+        System.out.println ("" + 0 += 10);
+        System.out.println ("" + 10 -= 10);
+        System.out.println ("" + 4 *== 50);
+        System.out.println ("" + 30 /= 11);
+        System.out.println ("Eu tenho", 15 anos);
+        System.out.println ("" + 30 x &= 3);
+        System.out.println ("" + 310 |*= 121);
+        System.out.println ("", 3 |= 112);
+    }
+    
+}
