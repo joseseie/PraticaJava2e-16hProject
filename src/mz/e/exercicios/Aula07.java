64a277f82c2c55620cf37cfaf8db39a52c8c24f3
aly <alymacome@gmail.com>
2019-12-10 05:07:14 +0200

    Resolucao de exercicios

---
+++ src/mz/e/exercicios/Aula07.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.e.exercicios;
+
+/**
+ *
+ * @author Aly Macome
+ */
+public class Aula07 {
+    
+    public static void main(String[]args){
+    
+        /**
+         * 3-Crie um array capaz de armazenar 50 numeros,em seguida faca o seu preenchimento automatico 
+         * com os numeros  101 a 150, ou seja ponha na posicao 0 o valor 101, na posicao 1 o valor 102,
+         * assim sucessivamente e por fim emprima os valores desse vector
+         */
+        //System.out.println("hh");
+        
+        int x [] = new int[50];
+        //int x = 10;
+        for (int j = 0; j < x.length; j++){            
+          System.out.println(x[j]);
+          //System.out.println(i);
+        }
+        
+        //String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        //for (int i = 0; i < cars.length; i++) {
+          //System.out.println(cars[i]);
+        
+    }
+    
+    
+}
