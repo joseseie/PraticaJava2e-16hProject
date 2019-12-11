1fa7001f3c73155b61eb8a1667ce38970a389495
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-11 20:04:52 +0200

    Resolução dos exercícios 01 e 05 da ficha de arrays

---
+++ src/mz/e/exercicios/Correcao01.java
@@ -0,0 +1,50 @@
+
+package mz.e.exercicios;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author joseseie
+ */
+public class Correcao01 {
+    
+    /*
+    1. O sistema de admissão à UEM da maior parte dos cursos de engenharias 
+    é composto por dois exames, Matemática e Física. Supondo que o primeiro 
+    exame tem peso 70%, e o segundo tem peso 30%. Faça um porgrama para 
+    calcular a média final de 10 alunos nestas disciplinas.
+
+    */
+    public static void main(String[] args) {
+        
+        // # Algoritmo
+        // 1. Media de cada aluno
+        // 2. Media final de 10 alunos.
+        
+        double[] medias = new double[3];
+        
+        Scanner s = new Scanner(System.in);
+        
+        for (int i = 0; i < medias.length; i++) {
+            
+            System.out.print("Nota de Matemática, para o aluno[" + i + "] : ");
+            double notaMat = s.nextDouble();
+            
+            System.out.print("Nota de Fisica, para o aluno[" + i + "] : ");
+            double notaFisica = s.nextDouble();
+            
+            System.out.println("");
+            
+            double media = notaMat * 0.7 + notaFisica * 0.3;
+            medias[i] = media;
+            
+        }
+        
+        System.out.println("Medias: " + Arrays.toString(medias));
+        
+        
+    }
+    
+}
---
+++ src/mz/e/exercicios/Correcao05.java
@@ -0,0 +1,39 @@
+
+package mz.e.exercicios;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author joseseie
+ */
+public class Correcao05 {
+    
+    /*
+        Criar dois arrays A e B cada um com 10 elementos inteiros. 
+        Construir um array C, onde cada elemento de C é a soma dos respectivos 
+        elementos em A e B, ou seja:
+        
+        C[i] = A[i] + B[i].
+    */
+    public static void main(String[] args) {
+        
+        // # Algoritmo:
+        //1. Criar dois arrays A e B e inicializar com 10 elementos inteiros
+        //2. Construir o array C.
+        //3. Somar os valores de cada posicao de A e B e guardar no C.
+        
+        int[] A = {2, 1, 8, 3, 8, 12, 10, 2, 7, 9};
+        int[] B = {3, 12, 86, 13, 28, 2, 0, 12, 47, 90};
+        
+        int[] C = new int[A.length];
+        
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i] + B[i];
+        }
+        
+        System.out.println("Array C: " + Arrays.toString(C));
+        
+    }
+    
+}
