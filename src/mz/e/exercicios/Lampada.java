1f6fc6ab42618f1d2ef1f50a05e1fc5b9cf89ca6
Moises <moisesmatsinhe>
2019-12-12 12:22:43 +0200

    Criacao da Classe Lampada

---
+++ src/mz/e/exercicios/Lampada.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mz.e.exercicios;
+
+/**
+ * Crie uma classe Lâmpada cujos atributos são: cor, estado, tipo, voltagem, grau
+ * de luminosidade (de 0 a 100)
+ */
+public class Lampada {
+    String cor;
+    String estado;
+    String tipo;
+    String voltagem;
+    int luminosidade;
+
+}
