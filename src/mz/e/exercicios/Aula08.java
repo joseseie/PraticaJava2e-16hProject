082fe7da87bb27cd19c8ee633d6deca65a428ffd
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-12 12:08:22 +0200

    Exercícios da aula 08

---
+++ src/mz/e/exercicios/Aula08.java
@@ -0,0 +1,110 @@
+
+package mz.e.exercicios;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author joseseie
+ * @author Filipe Mangue
+ * @author Horcha Chambule
+ */
+public class Aula08 {
+    
+    public static void main(String[] args) {
+        
+        List l = new List();
+        ArrayList<String> al = new ArrayList<>(5);
+        Vector v = new Vector(5, 5);
+        
+        // a)
+        v.add(..v);
+        
+        // b)
+        al.add(31 anos);
+        
+        // c)
+        al.add(10)
+             
+        // d)        
+        l.clean();
+        
+        // e)
+        ArrayList<init> ss;
+        
+        // f)
+        ss.add('Teste');
+        
+        // g)
+        lista.addicionar();
+        
+        //h)
+        Vector<Double> dar;
+        
+        // i)
+        dar.add();
+        
+        // j)
+        dar = new Vector<>()
+             
+                
+        // k)
+        dar.cleaaarar();
+        
+        // l)
+        v.add(dd)
+        
+        // m)
+        ss.addAll(v);
+        
+        // n)
+        ss.isEmpt();
+             
+        // o)
+        al.remove(---);
+        
+        // p)
+        al.aIndexOf(v);
+        
+        // q)
+        al.length;
+        
+        //r)
+        lista.removeAll()
+        
+        
+        //s)
+        array.pop()
+        
+        // t)
+        meuArrar.rem;
+        
+        // u)
+        Arrays.asList(al);
+        
+        // v)
+        Arrays.toString(lss.toArray())
+        
+        // w)
+        array = new ArrayList();
+        
+        // x)
+        vectorLKC = new Vect;
+        
+        // y)
+        vectorLKC.pop;
+        
+        
+        // y)
+        arrar.clean;
+        
+        
+        // z)
+        array.remove();
+        
+    }
+    
+}
