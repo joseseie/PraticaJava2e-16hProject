e498180466e3f62edd29eddccb3b75969505d45b
Efraime Mutole <58521063+mutole@users.noreply.github.com>
2019-12-12 16:24:17 +0200

    modificacao nos exercicios

---
+++ src/Exercicosaula7/Exercicio2.java
@@ -0,0 +1,21 @@
+/*
+2. Escreva um programa que leia 10 valores reais do teclado e 
+armazene-os num array d e permita que sejam impressos quanto solicitados. 
+ */
+package Exercicosaula7;
+
+import java.io.*;
+public class Exercicio2 {
+    
+    public static byte a(byte min, byte max) throws IOException
+    {
+         BufferedReader x= new BufferedReader(new InputStreamReader(System.in));
+         
+    }
+   
+    public static void main(String args []) throws IOException
+    {
+        BufferedReader x= new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
