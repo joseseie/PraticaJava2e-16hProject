5600b0ca5e81e56d657adf8b1671b2aacf895b42
Efraime Mutole <58521063+mutole@users.noreply.github.com>
2019-12-18 19:40:32 +0200

    lampada

--- Lampada/src/lampada/Lampada.java
+++ Lampada/src/lampada/Lampada.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 1. Crie uma classe Lâmpada cujos atributos são: cor, estado, tipo, voltagem, grau de luminosidade (de 0 a 100)
+a) Adicione os construtores necessários 
+b) Adicione os getters e setters 
+c) Adicione o método toString 
+d) Adicione métodos para ligar, desligar a luz 
+e) Adicione um método que mostre a mensagem “Luz Ligada” ou “Luz Desligada”. 
+f) Adicione um método que verifica se a lâmpada esta fundida 
+g) Adicione um método para verificar se 2 lâmpadas têm a mesma voltagem 
+h) Adicione um método que verifica se 2 lâmpadas são iguais 
+i) Adicione um método que aumente ou reduz o grau de luminosidade (de 5 em  
+j) Quando uma lâmpada ultrapassa o grau de luminosidade máximo (100) 
+funde (isto é, não pode ligar ou desligar mais). 
+1.1 Crie uma classe para testar a classe acima. 
+ 
  */
 package lampada;
 
@@ -16,6 +27,9 @@ public class Lampada {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Lampada1 lam= new Lampada1();
+        lam.setCor("VErmehlo");
+        lam.visualizacoes();
     }
     
 }
---
+++ Lampada/src/lampada/Lampada1.java
@@ -0,0 +1,75 @@
+/*
+  1. Crie uma classe Lâmpada cujos atributos são: cor, estado, tipo, voltagem, grau de luminosidade (de 0 a 100)
+a) Adicione os construtores necessários 
+b) Adicione os getters e setters 
+c) Adicione o método toString 
+d) Adicione métodos para ligar, desligar a luz 
+e) Adicione um método que mostre a mensagem “Luz Ligada” ou “Luz Desligada”. 
+f) Adicione um método que verifica se a lâmpada esta fundida 
+g) Adicione um método para verificar se 2 lâmpadas têm a mesma voltagem 
+h) Adicione um método que verifica se 2 lâmpadas são iguais 
+i) Adicione um método que aumente ou reduz o grau de luminosidade (de 5 em  
+j) Quando uma lâmpada ultrapassa o grau de luminosidade máximo (100) 
+funde (isto é, não pode ligar ou desligar mais). 
+1.1 Crie uma classe para testar a classe acima. 
+ */
+package lampada;
+
+/**
+ *
+ * @author Mutole
+ */
+public class Lampada1 {
+    String cor;
+    String tipo;
+    float voltagem;
+    float luminosidade;
+    boolean estado;
+    
+    void visualizacoes(){
+        System.out.println("A cor da lampada e'" +getCor());
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(float voltagem) {
+        this.voltagem = voltagem;
+    }
+
+    public float getLuminosidade() {
+        return luminosidade;
+    }
+
+    public void setLuminosidade(float luminosidade) {
+        this.luminosidade = luminosidade;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
